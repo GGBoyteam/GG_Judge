86af25f58178f503c14374437a52c317728f5ec7
mc23 <2963456487@qq.com>
2023-08-29 22:48:18 +0800

    Signed-off-by: mc23 <2963456487@qq.com>

---
+++ deploylocal/start.sh
@@ -0,0 +1,38 @@
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+				  
+yum install -y yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+	
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable docker
+
+mkdir -p /etc/docker
+
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://qkjfxye4.mirror.aliyuncs.com"]
+}
+EOF
+
+
+systemctl daemon-reload
+
+
+systemctl restart docker
+
+
+cd `dirname $0`
+
+
+docker build -f 
