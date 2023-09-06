dcee1d431f22dc8985257eebd02f00ee51bd58f0
mc23 <2963456487@qq.com>
2023-09-06 20:50:10 +0800

    代码重构最终版

--- GG_auth/src/main/java/com/zhangsiyao/auth/component/Md5Component.java
+++ GG_auth/src/main/java/com/zhangsiyao/auth/component/Md5Component.java
@@ -2,9 +2,11 @@ package com.zhangsiyao.auth.component;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 public class Md5Component {
 
     @Value("${md5.salt:zhangsiyao}")
